a82182fada951c4aa8b8a9b8449284e81dcf117c
Ethan Chu <ethanchu@mikes-mbp.lan>
2020-09-12 17:59:06 -0700

    Week 1

---
+++ src/HW1Song.java
@@ -0,0 +1,49 @@
+
+public class HW1Song {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("There was an old woman who swallowed a fly.");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.\n");
+		System.out.println("There was an old woman who swallowed a spider,");
+		System.out.println("That wriggled and iggled and jiggled inside her.");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.\n");
+		System.out.println("There was an old woman who swallowed a bird,");
+		System.out.println("How absurd to swallow a bird.");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.\n");
+		System.out.println("There was an old woman who swallowed a cat,");
+		System.out.println("Imagine that to swallow a cat.");
+		System.out.println("She swallowed the cat to catch the bird,");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.\n");
+		System.out.println("There was an old woman who swallowed a dog,");
+		System.out.println("What a hog to swallow a dog.");
+		System.out.println("She swallowed the dog to catch the cat,");
+		System.out.println("She swallowed the cat to catch the bird,");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.\n");
+		System.out.println("There was an old woman who swallowed a bear");
+		System.out.println("Every last hair, she swallowed that bear.");
+		System.out.println("She swallowed the bear to catch the dog,");
+		System.out.println("She swallowed the dog to catch the cat,");
+		System.out.println("She swallowed the cat to catch the bird,");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.\n");
+		System.out.println("There was an old woman who swallowed a horse,");
+		System.out.println("She died of course.");
+
+	}
+
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello \n");
+		System.out.print("This is my first class \n");
+		System.out.print("Jane Campion directed \"The Piano\" in 1993. \n");
+		String firstname;
+		firstname = "Ethan Chu";
+		System.out.print(firstname);
+
+	}
+
+}
